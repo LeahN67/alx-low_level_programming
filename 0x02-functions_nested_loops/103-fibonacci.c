47f9d8f6a40503c7589a2612002a6703bdd73923
Leah Njuguna <munjiruai@gmail.com>
2023-10-13 13:24:08 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main- function to find and print the sum of the eve-valued terms
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	unsigned long int a, b, next, sum;
+
+	a = 1;
+	b = 2;
+	sum = 0;
+
+	for (n = 1; n <= 33; ++n)
+	{
+		if (a < 4000000 && (a %2) == 0)
+		{
+			sum = sum + a;
+		}
+		next = a + b;
+		a = b;
+		b = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
