b8421c4b0d22934a14c87ffded3628b070081446
Leah Njuguna <munjiruai@gmail.com>
2023-10-13 03:23:57 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for n == 0 or n > 0
+ *
+ * @n: number to be checked
+ *
+ * Return: Always 0
+ */
+int _print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
