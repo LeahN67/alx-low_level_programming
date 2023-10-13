6cf622bd4c8b3af067ec21b2e50f303d6736b440
Leah Njuguna <munjiruai@gmail.com>
2023-10-13 16:02:03 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the selected numbers,
+ *followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c !=2 && c !=4)
+		{
+			_putchar(i + '0')
+		}
+	}
+
+	_putchar('\n')
+}
