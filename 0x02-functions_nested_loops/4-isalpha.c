5d7b7b84d9f30b398560d6218a92eff147c8bcde
Leah Njuguna <munjiruai@gmail.com>
2023-10-13 02:50:25 -0700

    isalphawq

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha(int c) - checks for lowercase or uppercaser character
+ *
+ * @c:letter to be checked
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
