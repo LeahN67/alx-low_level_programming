d7e4987382ae167864ec68cfaf61c52279fdace5
Leah Njuguna <munjiruai@gmail.com>
2023-10-13 07:24:15 -0700

    a+ b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @c: integer to be added to d
+ *
+ * @d: integer to be added to c
+ *
+ * Return: sum of c and d
+ */ 
+
+
+int  add(int c, int d )
+{
+	int e;
+
+	e = c + d;
+
+	return (e);
+}
