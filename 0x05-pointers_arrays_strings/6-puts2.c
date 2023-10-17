3ad1524644987e5e6dd29141018958fc9cc456ac
Leah Njuguna <munjiruai@gmail.com>
2023-10-17 05:54:39 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * Description: to print one character of 2 of a string
+ * followed by new line
+ *
+ * @str: pointer of the string to print the characters from
+ */
+void puts2(char *str)
+{
+	int length;
+	int i;
+
+	length = 0;
+
+	while (str[len] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i +=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n')
+}
