9cfd9403e454af896eacba4a835532e510c8fdda
Leah Njuguna <munjiruai@gmail.com>
2023-10-17 09:10:09 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ *
+ * Description: converting a string into an integer
+ *
+ * @s: the string to be used
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int j = 0;
+	unsigned int result = 0;
+
+	while (!(s[j] <= '9' && s[j] >= '0' && s[j] != '\0'))
+	{
+		result = (result * 10) + (s[j] - '0');
+		j++;
+	}
+
+	while (s[i] <= '9' && (s[j] >= '0' && s[i] != '\0'))
+	{
+		result = (result * 10) + (s[j] - '0');
+		i++;
+	}
+	rsult *= sign;
+	return (result);
+}
+
