61a9a4f80d4d9e0958a29189909a3b42fe46b47b
Leah Njuguna <munjiruai@gmail.com>
2023-10-17 05:10:02 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ *
+ * Description: prints a string to stdout
+ *
+ * @str: pointer to the string to be printed
+ */
+void _puts(char *str)
+{
+	int j;
+
+	for (j = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
