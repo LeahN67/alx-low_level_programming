ccea82f58b0375bff1c07059726a196aeeb30604
Leah Njuguna <munjiruai@gmail.com>
2023-10-19 09:09:49 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,10 +18,10 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 == *s2 && *s1 != '\0')
 	{
 		s1++;
-		s2++
+		s2++;
 	}
 	if (s1 != s2)
 		results = *s1 - *s2;
 
-	return (result)
+	return (result);
 }
