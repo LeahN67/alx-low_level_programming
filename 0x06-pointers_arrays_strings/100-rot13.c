b07165cd37bfdb37fe127a8cb5cb5e616825d1e2
Leah Njuguna <munjiruai@gmail.com>
2023-10-19 13:38:10 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encoding a string in rot13
+ * @s: string to encode
+ *
+ * Return: the resultant string
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
