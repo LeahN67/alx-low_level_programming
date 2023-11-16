560749b33f96328b167f489476d9840dc1a084da
Leah Njuguna <munjiruai@gmail.com>
2023-11-16 11:30:13 -0800

    add node

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - returns elements in a linked list
+ * @h: linked list
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
